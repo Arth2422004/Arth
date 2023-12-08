d9250613c06f6a95bc6e2b57f4d777cc811a1481
Arth2422004 <153264616+Arth2422004@users.noreply.github.com>
2023-12-08 14:03:27 +0530

    Add files via upload

---
+++ ArthLanguage.c/pattern.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+       printf("\n        *\t  *");
+       printf("\n   *\t     *");
+     printf("\n*       *");
+     printf("\n   *\t     *");
+     printf("\n        *\t  *");
+    return 0;
+}
---
+++ ArthLanguage.c/salary.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(){
+    int  y,hra,da,ta;
+    printf("enter slary");
+    scanf("%d",&y);
+
+
+    printf("\nhra %d", hra=y * 10/100);
+     printf("\nda %d", da=y * 10/100);
+      printf("\nta %d", ta=y * 10/100);
+  	return 0;
+}
